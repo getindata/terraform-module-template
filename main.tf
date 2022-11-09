07ef26e9f722295093f49881c6c10a43471551ed
Jakub Igla <jakub.igla@getindata.com>
2022-11-09 16:55:38 +0100

    chore: Change examples folder, move examples section from docs yaml

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 # echoes it's base64 encoded version locally 
 
 resource "null_resource" "output_input" {
-
   triggers = {
+    name  = local.name_from_descriptor
     input = var.example_var
   }
 
