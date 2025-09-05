ace33057519328b932dc2a914a9631b7d926a8d9
Dominik Gniewek-Węgrzyn <dominik.gniewek@getindata.com>
2025-09-05 14:41:16 +0200

    chore: lint

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   context_template = lookup(var.context_templates, var.name_scheme.context_template_name, null)
-  subresource_context_template = lookup(var.context_templates, 
-    coalesce(var.sub_resource.context_template_name, var.name_scheme.context_template_name), 
+  subresource_context_template = lookup(var.context_templates,
+    coalesce(var.sub_resource.context_template_name, var.name_scheme.context_template_name),
     null
   )
 }
